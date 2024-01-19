b8deaad15136bab2ee19a4afb17821ddfa30d00b
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2024-01-19 18:32:42 +0100

    committing JavaDoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fhtw"},{"l":"fhtw.chat"},{"l":"fhtw.data"},{"l":"fhtw.message"},{"l":"fhtw.user"}];updateSearchResults();
